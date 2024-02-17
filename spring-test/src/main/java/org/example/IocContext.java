1d057bd1f46f7137386a6c92b18f1a24ebd97449
troy.chen <492794628@qq.com>
2024-02-17 20:58:33 +0800

    spring tiaoz

---
+++ spring-test/src/main/java/org/example/IocContext.java
@@ -0,0 +1,61 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IocContext implements ApplicationContextAware {
+    /**
+     * 上下文对象实例
+     */
+    private static ApplicationContext applicationContext;
+
+    @Autowired
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        IocContext.applicationContext = applicationContext;
+    }
+
+    /**
+     * 获取applicationContext
+     */
+    public static ApplicationContext getApplicationContext() {
+        applicationContext.getAutowireCapableBeanFactory()
+        return IocContext.applicationContext;
+    }
+
+    /**
+     * 通过name获取 Bean.
+     *
+     * @param name 参数传入要获取的实例的类名 首字母小写，这是默认的
+     * @return
+     */
+    public static Object getBean(String name) {
+        return getApplicationContext().getBean(name);
+    }
+
+    /**
+     * 通过class获取Bean.
+     *
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(Class<T> clazz) {
+        return getApplicationContext().getBean(clazz);
+    }
+
+    /**
+     * 通过name,以及Clazz返回指定的Bean
+     *
+     * @param name
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(String name, Class<T> clazz) {
+        return getApplicationContext().getBean(name, clazz);
+    }
+
+}
--- spring-test/src/main/java/org/example/Main.java
+++ spring-test/src/main/java/org/example/Main.java
@@ -3,17 +3,18 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.io.IOException;
 
 @EnableAsync
 @ConfigurationPropertiesScan
-@SpringBootApplication/*spring 启动标记*/
+@SpringBootApplication(exclude = {})/*spring 启动标记*/
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        SpringApplication.run(Main.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(Main.class, args);
     }
 
 }
