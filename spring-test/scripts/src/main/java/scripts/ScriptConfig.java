3adf0486fea6d16fd8897303b1e3d587f92a8f7f
wuxin <492794628@qq.com>
2024-02-18 21:38:11 +0800

    重构目录

--- spring-test/scripts/src/main/java/scripts/ScriptConfig.java
+++ spring-test/scripts/src/main/java/scripts/ScriptConfig.java
@@ -1,21 +1,13 @@
 package scripts;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@RestController
-public class TestApi {
-
-    @RequestMapping(value = "/test")
-    public String test() {
-        return "ok";
-    }
+@Configuration
+public class ScriptConfig {
 
     @Bean
     public ServletRegistrationBean resourceServlet(ApplicationContext applicationContext) {
--- spring-test/scripts/src/main/java/scripts/ScriptMain.java
+++ spring-test/scripts/src/main/java/scripts/ScriptMain.java
@@ -1,34 +1,22 @@
-package org.example;
+package scripts;
 
+import org.example.HelloController;
+import org.example.Main;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import scripts.TestApi;
 
-import java.io.IOException;
 import java.util.Map;
 
-@EnableAsync
-@ConfigurationPropertiesScan
-@SpringBootApplication/*spring 启动标记*/
-public class Main {
+public class ScriptMain {
 
-    static ConfigurableApplicationContext applicationContext;
     static AnnotationConfigApplicationContext childContainer;
 
-    public static void main(String[] args) throws IOException {
-
-        applicationContext = SpringApplication.run(Main.class, args);
-
+    public static void scriptInit(ConfigurableApplicationContext context) {
         //子容器
         childContainer = new AnnotationConfigApplicationContext("scripts");
-        childContainer.setParent(applicationContext);
-        childContainer.setApplicationStartup(applicationContext.getApplicationStartup());
+        childContainer.setParent(context);
+        childContainer.setApplicationStartup(context.getApplicationStartup());
         //从子容器中获取父容器中的Bean
         HelloController parentService = childContainer.getBean(HelloController.class);
         LoggerFactory.getLogger(Main.class).info("{}", parentService);
@@ -37,7 +25,6 @@ public class Main {
         map.forEach((k, v) -> LoggerFactory.getLogger(Main.class).info("{} => {}", k, v));
 
         System.out.println("http://127.0.0.1:18081/test");
-
     }
 
 }
