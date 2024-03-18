e7425ff907f65ae05b1430155657cc52bc390dfc
wxd-gaming <492794628@qq.com>
2024-03-18 19:55:29 +0800

    【测试代码】

---
+++ wxdgaming-mmoarpg/wxdgaming.mmo.web/src/main/resources/html/js/tipstype.js
@@ -0,0 +1,25 @@
+// source: Tips.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
+
+goog.provide('proto.Proto.Tips.TipsType');
+
+/**
+ * @enum {number}
+ */
+proto.Proto.Tips.TipsType = {
+  NORMAL: 0,
+  TOP: 1,
+  CHAT: 2,
+  ERROR: 3
+};
+
