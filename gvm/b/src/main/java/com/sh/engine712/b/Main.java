7796f41369949f6c845942c2f70d1be539ec0ed2
wxd-gaming <492794628@qq.com>
2024-07-04 21:06:01 +0800

    【优化】各种编译问题

--- gvm/b/src/main/java/com/sh/engine712/b/Main.java
+++ gvm/b/src/main/java/com/sh/engine712/b/Main.java
@@ -1,7 +1,16 @@
 package com.sh.engine712.b;
 
 public class Main {
+
     public static void main(String[] args) {
         com.sh.engine712.a.Main.print();
+        ClassLoader classLoader = Main.class.getClassLoader();
+        ReflectContext.Builder
+                .of(classLoader, "com.sh.engine712").build()
+                .classStream()
+                .forEach(System.out::println);
+        System.out.println(classLoader);
     }
+
+
 }
