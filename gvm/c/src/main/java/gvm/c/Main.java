571a2e0b63c56fe013d1e22ad8ecafc9513c0a0f
wxd-gaming <492794628@qq.com>
2024-07-05 21:18:51 +0800

    【优化】pom引用

--- gvm/c/src/main/java/gvm/c/Main.java
+++ gvm/c/src/main/java/gvm/c/Main.java
@@ -1,35 +1,44 @@
 package gvm.c;
 
-import wxdgaming.boot.agent.loader.RemoteClassLoader;
 import wxdgaming.boot.agent.system.ReflectContext;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.lang.reflect.Method;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        RemoteClassLoader remoteClassLoader = RemoteClassLoader.build(
-                Main.class.getClassLoader(),
-                "http://127.0.0.1:19000/serverapi/s1/aabb/128493823/a-encrypted.jar",
-                "http://127.0.0.1:19000/serverapi/s1/aabb/128493823/b-encrypted.jar"
-        );
-
+        // RemoteClassLoader remoteClassLoader = RemoteClassLoader.build(
+        //         Main.class.getClassLoader(),
+        //         "http://127.0.0.1:19000/serverapi/s1/aabb/128493823/a-encrypted.jar",
+        //         "http://127.0.0.1:19000/serverapi/s1/aabb/128493823/b-encrypted.jar"
+        // );
+        ClassLoader classLoader = Main.class.getClassLoader();
         ReflectContext.Builder
-                .of(remoteClassLoader, "gvm").build()
+                .of(classLoader, "gvm").build()
                 .classStream()
                 .forEach(System.out::println);
 
-        System.out.println(remoteClassLoader);
+        System.out.println(classLoader);
 
-        Class<?> aClass = remoteClassLoader.loadClass("gvm.b.Main");
-        System.out.println(aClass.getClassLoader().hashCode());
-        System.out.println(aClass.hashCode());
+        Set<Class<?>> classes = SubstrateLoadedClassTable.getLoadedClasses();
+        for (Class<?> clazz : classes) {
+            if (!clazz.getName().startsWith("java.") && !clazz.getName().startsWith("sun.") && !clazz.getName().startsWith("com.oracle")) {
+                System.out.println(clazz.getName());
+            }
+        }
 
-        Method point = aClass.getMethod("point");
-        point.invoke(aClass.getConstructor().newInstance());
+        Class<?> aClass = classLoader.loadClass("gvm.b.Main");
+        if (aClass != null) {
+            System.out.println(aClass.getClassLoader().hashCode());
+            System.out.println(aClass.hashCode());
+
+            Method point = aClass.getMethod("point");
+            point.invoke(aClass.getConstructor().newInstance());
+        }
         System.out.println("敲入回车关闭：");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         reader.readLine();
---
+++ gvm/package-graalvm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 设置JAVA_HOME环境变量
+export JAVA_HOME=/usr/local/graalvm-jdk-21.0.3+7.1
+# 将JAVA_HOME加入到PATH变量中
+export PATH=$JAVA_HOME/bin:$PATH
+# 打印出Java版本信息以验证设置是否成功
+java -version
+#cd /mnt/e/work/engine712-server
+mvn clean package -f pom.xml
+
+cd c/target
+#/usr/local/graalvm-jdk-21.0.3+7.1/bin/java -agentlib:native-image-agent=config-merge-dir=c/src/resources/META-INF/native-image -jar server-boot.jar
+
+./gvm-test
