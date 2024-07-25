3778269c9e8879397c77b79b0e580408fcd1fb79
wxd-gaming <492794628@qq.com>
2024-07-25 21:07:40 +0800

    【新增】测试。调试代码

--- spring-test/scripts/src/main/java/scripts/TestApi2.java
+++ spring-test/scripts/src/main/java/scripts/TestApi2.java
@@ -1,20 +1,22 @@
 package scripts;
 
+import lombok.extern.slf4j.Slf4j;
 import org.example.HelloController;
-import org.example.Main;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
-public class TestApi {
+public class TestApi2 {
 
     @Autowired HelloController helloController;
+    @Autowired TestApi testApi;
 
-    @RequestMapping(value = "/test")
-    public String test() throws Exception {
-        Main.reload();
-        return "ok4";
+    @RequestMapping(value = "/test2")
+    public String test2() throws Exception {
+        log.debug("{}", testApi);
+        return "test2";
     }
 
 }
--- spring-test/server/src/main/java/org/example/Main.java
+++ spring-test/server/src/main/java/org/example/Main.java
@@ -3,22 +3,24 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import wxdgaming.boot.agent.loader.ClassDirLoader;
 
 @EnableAsync
+@EnableScheduling
 @ConfigurationPropertiesScan
 @SpringBootApplication/*spring 启动标记*/
 public class Main {
 
-    static ConfigurableApplicationContext applicationContext;
-
     public static void main(String[] args) throws Exception {
-        applicationContext = SpringApplication.run(Main.class, args);
-        // ClassDirLoader classDirLoader = new ClassDirLoader("scripts/target/classes", Main.class.getClassLoader());
-        // Class<?> aClass = classDirLoader.loadClass("scripts.ScriptMain");
-        // Method scriptInit = aClass.getMethod("scriptInit", ConfigurableApplicationContext.class, ClassDirLoader.class);
-        // scriptInit.invoke(null, applicationContext, classDirLoader);
+        IocContext.setApplicationContext(SpringApplication.run(Main.class, args));
+        reload();
+    }
+
+    public static void reload() throws Exception {
+        ClassDirLoader classDirLoader = new ClassDirLoader("scripts/target/classes", Main.class.getClassLoader());
+        IocContext.loadClassLoader(classDirLoader, "scripts");
     }
 
 }
