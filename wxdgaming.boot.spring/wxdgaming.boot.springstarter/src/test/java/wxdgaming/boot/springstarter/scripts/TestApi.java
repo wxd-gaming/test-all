538409f559df5f42fa38acf1585c26988325c097
wxdgaming <492794628@qq.com>
2024-07-28 12:13:40 +0800

    【新增】spring

--- wxdgaming.boot.spring/wxdgaming.boot.springstarter/src/test/java/wxdgaming/boot/springstarter/scripts/TestApi.java
+++ wxdgaming.boot.spring/wxdgaming.boot.springstarter/src/test/java/wxdgaming/boot/springstarter/scripts/TestApi.java
@@ -1,13 +1,14 @@
 package wxdgaming.boot.springstarter.scripts;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.luaj.vm2.lib.jse.CoerceJavaToLua;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import wxdgaming.boot.LuaBus;
 import wxdgaming.boot.springstarter.test.B1;
 
 /**
@@ -23,6 +24,26 @@ public class TestApi {
     @Autowired B1 b1;
     @Autowired B2 b2;
 
+    LuaBus luaBus;
+
+    @PostConstruct
+    public void init() {
+        luaBus = LuaBus.buildFromDirs("lua");
+
+    }
+
+    @RequestMapping("/**")
+    public ResponseEntity<?> all(
+            HttpServletRequest httpServletRequest,
+            HttpServletResponse httpServletResponse,
+            @RequestBody(required = false) String body) throws Exception {
+        luaBus.get("index").call(
+                CoerceJavaToLua.coerce(httpServletRequest),
+                CoerceJavaToLua.coerce(httpServletResponse)
+        );
+        return ResponseEntity.ok("ok-" + b1);
+    }
+
     @RequestMapping("/api")
     public ResponseEntity<?> api(
             HttpServletRequest httpServletRequest,
@@ -30,6 +51,11 @@ public class TestApi {
             @RequestBody(required = false) String body) throws Exception {
 
         return ResponseEntity.ok("ok-" + b1);
+    }
+
+    @GetMapping("/users/{userId}")
+    public String getUserById(@PathVariable String userId) {
+        return "User Id is: " + userId;
     }
 
 }
