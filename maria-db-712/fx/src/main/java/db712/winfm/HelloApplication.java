856d87bffeb75d04d9bf83c1aa1552f305d95eee
wxd-gaming <492794628@qq.com>
2024-08-14 11:07:48 +0800

    【优化】关闭按钮新增确认框

--- maria-db-712/fx/src/main/java/db712/winfm/HelloApplication.java
+++ maria-db-712/fx/src/main/java/db712/winfm/HelloApplication.java
@@ -7,6 +7,9 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -21,6 +24,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Image image_logo = new Image("logo.png");
         /*阻止停止运行*/
         Platform.setImplicitExit(false);
         WebService.getIns().setShowWindow(() -> {
@@ -40,49 +44,70 @@ public class HelloApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load(), Color.BLACK);
         primaryStage.setTitle("qj-712-db-server");
         primaryStage.setScene(scene);
-        Image image = new Image("logo.png");
-        primaryStage.getIcons().add(image);
+
+        primaryStage.getIcons().add(image_logo);
         // primaryStage.initStyle(StageStyle.UNDECORATED);    // 可以隐藏任务栏上的图标
         primaryStage.setOnCloseRequest(windowEvent -> {
             windowEvent.consume();
-            primaryStage.setIconified(true);
-            primaryStage.hide();
-            System.out.println("禁止关闭");
+            select(primaryStage);
         });
         /*todo 通过调用 http://localhost:19902/api/db/show */
         primaryStage.show();
         primaryStage.setAlwaysOnTop(true);
         primaryStage.setAlwaysOnTop(false);
-        try {
-            Properties properties = new Properties();
-            properties.load(Files.newInputStream(Paths.get("my.ini")));
-            WebService.getIns().setPort(Integer.parseInt(properties.getProperty("web-port")));
-            DBFactory.getIns().init(
-                    properties.getProperty("database"),
-                    Integer.parseInt(properties.getProperty("port")),
-                    properties.getProperty("user"),
-                    properties.getProperty("pwd")
-            );
-            DBFactory.getIns().print();
-            WebService.getIns().start();
-            WebService.getIns().initShow();
-            CompletableFuture.runAsync(() -> {
+        CompletableFuture.runAsync(() -> {
+            try {
+                Thread.sleep(500);
+                Properties properties = new Properties();
+                properties.load(Files.newInputStream(Paths.get("my.ini")));
+                WebService.getIns().setPort(Integer.parseInt(properties.getProperty("web-port")));
+                DBFactory.getIns().init(
+                        properties.getProperty("database"),
+                        Integer.parseInt(properties.getProperty("port")),
+                        properties.getProperty("user"),
+                        properties.getProperty("pwd")
+                );
+                DBFactory.getIns().print();
+                WebService.getIns().start();
+                WebService.getIns().initShow();
 
-                try {
-                    Thread.sleep(10_000);
-                    PlatformImpl.runAndWait(() -> {
-                        primaryStage.hide();
-                    });
-                } catch (Exception ignore) {}
+                CompletableFuture.runAsync(() -> {
 
-            });
-        } catch (Exception e) {
-            e.printStackTrace(System.out);
-            System.out.println("启动异常了！");
-            System.out.println("启动异常了！");
-            System.out.println("启动异常了！");
-            System.out.println("启动异常了！");
-        }
+                    try {
+                        Thread.sleep(10_000);
+                        PlatformImpl.runAndWait(() -> {
+                            primaryStage.hide();
+                        });
+                    } catch (Exception ignore) {}
+
+                });
+            } catch (Throwable e) {
+                e.printStackTrace(System.out);
+                System.out.println("启动异常了！");
+                System.out.println("启动异常了！");
+                System.out.println("启动异常了！");
+                System.out.println("启动异常了！");
+            }
+        });
     }
 
+    public void select(Stage primaryStage) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("提示");
+        alert.setHeaderText("点击按钮以确认你的选择!");
+        // alert.setContentText("点击按钮以确认你的选择。");
+
+        ButtonType buttonTypeOk = new ButtonType("关闭进程", ButtonBar.ButtonData.YES);
+        ButtonType buttonTypeCancel = new ButtonType("后台运行", ButtonBar.ButtonData.OK_DONE);
+
+        alert.getButtonTypes().setAll(buttonTypeCancel, buttonTypeOk);
+
+        ButtonType result = alert.showAndWait().orElse(buttonTypeCancel);
+
+        if (result == buttonTypeOk) {
+            Runtime.getRuntime().exit(0);
+        } else {
+            primaryStage.hide();
+        }
+    }
 }
--- maria-db-712/fx/src/main/java/db712/winfm/HelloController.java
+++ maria-db-712/fx/src/main/java/db712/winfm/HelloController.java
@@ -3,6 +3,9 @@ package db712.winfm;
 import db712.server.DBFactory;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 import lombok.extern.slf4j.Slf4j;
 
@@ -31,16 +34,21 @@ public class HelloController {
                     /*TODO 必须要等他初始化完成*/
                     PrintStream printStream = new PrintStream(System.out) {
                         @Override public void print(String x) {
-                            Platform.runLater(() -> {
-                                /*委托给ui线程*/
-                                try {
-                                    while (list_view.getItems().size() > 1000) {
-                                        list_view.getItems().removeFirst();
-                                    }
-                                    list_view.getItems().add(x);
-                                    list_view.scrollTo(list_view.getItems().size() + 999);
-                                } catch (Throwable ignore) {}
-                            });
+                            try {
+                                Platform.runLater(() -> {
+                                    String line = x;
+                                    /*委托给ui线程*/
+                                    try {
+                                        while (list_view.getItems().size() > 300) {
+                                            list_view.getItems().removeFirst();
+                                        }
+                                        if (line.length() > 200)
+                                            line = line.substring(0, 200) + "......";
+                                        list_view.getItems().add(line);
+                                        list_view.scrollTo(list_view.getItems().size() + 999);
+                                    } catch (Throwable ignore) {}
+                                });
+                            } catch (Throwable ignore) {}
                         }
                     };
                     System.setOut(printStream);
@@ -50,5 +58,7 @@ public class HelloController {
             }
         });
     }
+
+
 
 }
