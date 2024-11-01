41a7e5a5d4dc1f18ee13148f00867856a01335ba
wxd-gaming <492794628@qq.com>
2024-11-01 17:22:06 +0800

    【新增】题库测试

---
+++ lua-test/luac/src/test/java/J0002.java
@@ -0,0 +1,10 @@
+
+
+public class J0001 {
+
+    public static void main(String[] args) {
+        System.out.println("6 4");
+
+    }
+
+}
---
+++ lua-test/luac/src/test/java/J0003.java
@@ -0,0 +1,11 @@
+
+
+public class J0002 {
+
+    public static void main(String[] args) {
+        System.out.println(14 / 4);
+        System.out.println(14 - 14 % 4);
+        System.out.println(14 % 4);
+    }
+
+}
